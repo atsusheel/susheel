e15f9d158109fc3037afd2337487fc231814886d
atsusheel <atsusheel@gmail.com>
2019-03-01 12:08:58 +0530

    Revert "did few changes"
    This reverts commit 4858ebea15dd8a651c8f24d651610c212f085cef.

--- java test project/src/hdfcCarLoan/Projectclass1.java
+++ java test project/src/hdfcCarLoan/Projectclass1.java
@@ -3,33 +3,28 @@ package hdfcCarLoan;
 public class Projectclass1
 	{
 		int a,b,c;
-		
 		public Projectclass1(int a, int b) 
 		{
 			this.a=a;
 			this.b=b;
 			
 		}
-		
 		public void add()
 		{
 		  c = a + b;
-		  System.out.println("Add :" + c);
+		  System.out.println(c);
 		}
-		
 		public void sub()
 		{
-		  c = a - b;
-		  System.out.println("Add :" + c);
+			c = a - b;
+			System.out.println(c);
 			
 		}
-		
-	public static void main(String[] args) 
-		{
-			Projectclass1 w1 = new Projectclass1(20,10);
-			w1.add();
-			w1.sub();
-	
-		}
+	public static void main(String[] args) {
+		Projectclass1 w1 = new Projectclass1(20,10);
+		w1.add();
+		w1.sub();
+
+	}
 
 }
