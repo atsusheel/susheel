ad1d9d57da9dd8ca41cb1c871fb4d3cf014adcac
atsusheel <atsusheel@gmail.com>
2019-03-08 12:26:34 +0530

    add new programs

---
+++ Project1/src/project1/Table.java
@@ -0,0 +1,29 @@
+package project1;
+
+import java.util.Scanner;
+
+public class Table 
+{
+
+	public static void main(String[] args)
+	{
+		int n = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter integer number :");
+		
+		n = sc.nextInt();
+		
+		System.out.println("Table of " + n + " is :");
+		
+		for (int i = 1; i <= 10; i++)
+		{
+			
+			System.out.println(n*i);
+			
+		}
+
+	}
+
+}
---
+++ Project1/src/project1/WholeAndPrimeNumber.java
@@ -0,0 +1,33 @@
+package project1;
+
+public class WholeAndPrimeNumber
+{
+
+	public static void main(String[] args)
+	{
+			
+		int i = 1;
+					
+					while(i <= 100) {
+						if (i % 2 != 0) {
+							System.out.println(i);
+							
+						}
+					i++;
+					}
+					
+					
+				
+					
+					/*
+					System.out.println("Odd number is :");
+					for (int i = 0; i <= 100; i++) {
+						if (i % 2 != 0) {
+							System.out.println(i);
+						}
+					}
+					*/
+
+	}
+
+}
